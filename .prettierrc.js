f3a6f89ad32be3c1c219b09ab639b4e369ba3f29
vixer93 <sasasa.24.blue@gmail.com>
2021-03-20 01:27:34 +0900

    Add eslint

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    quoteProps: 'as-needed',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+};
